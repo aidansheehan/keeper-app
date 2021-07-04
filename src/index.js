2d2977ae6206809454f8c0b5d19970029574ae75
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-07-04 14:11:01 +0700

    use props and map to render notes dynamically from file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Note from "./Note.jsx";
+import App from "./components/App";
 
-
-ReactDOM.render(
-  <div>
-    <Header />
-    <Note />
-    <Footer />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
